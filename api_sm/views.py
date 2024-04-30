054b09dab36ff665d5f57a0b9b4f0da03c94b08d
djennaoui <djennaoui.raouf@gmail.com>
2024-04-30 15:49:36 +0100

    init

--- api_sm/views.py
+++ api_sm/views.py
@@ -485,13 +485,13 @@ class UpdateDQEApiVew(generics.UpdateAPIView):
 
 class UpdateNTApiVew(generics.UpdateAPIView):
     queryset = NT.objects.all()
-    serializer_class = DQESerializer
+    serializer_class = NTSerializer
     lookup_field = "id"
     def get_object(self):
         pk = self.request.data.get('id')
         try:
-            obj = NT.objects.get(id=id)
-        except DQE.DoesNotExist:
+            obj = NT.objects.get(id=pk)
+        except NT.DoesNotExist:
             raise NotFound("Object n'éxiste pas")
         self.check_object_permissions(self.request, obj)
 
